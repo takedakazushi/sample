317be1b273e130fb2588981ad73b39e8024027d0
takedakazushi <ec2-user@ip-172-31-44-28.ap-northeast-1.compute.internal>
2023-08-01 10:44:05 +0000

    追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'todolists/new'
   get 'top' => 'homes#top'
+  root to: 'homes#top'
   post 'todolists' => 'todolists#create'
   get 'todolists' => 'todolists#index'
   get 'todolists/:id' => 'todolists#show', as: 'todolist'
